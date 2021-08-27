09205ce154cae8a726f718d90347944bd48a7b0a
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-08-27 14:40:37 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { uuid } from "uuidv4";
 
-import TextEditor from "./TextEditor";
+import TextEditor from "./texteditor";
 
 function App() {
   return (
